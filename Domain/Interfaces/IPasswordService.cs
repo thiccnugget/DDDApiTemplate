af1d9e14164ea3cc67982c26da4ad041b161136c
thiccnugget <samuelecammaraa@gmail.com>
2024-12-04 23:33:28 +0100

    feat: added password service

---
+++ Domain/Interfaces/IPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IPasswordService
+    {
+        string GenerateSalt();
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
---
+++ Infrastructure/Services/BcryptPasswordService.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Services
+{
+
+    public class BcryptPasswordService : IPasswordService
+    {
+        private readonly string _pepper;
+        private const int WorkFactor = 15;
+
+        public BcryptPasswordService(string pepper)
+        {
+            _pepper = pepper ?? throw new ArgumentNullException(nameof(pepper));
+        }
+
+        public string GenerateSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(WorkFactor);
+        }
+
+        public string HashPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password.Concat(_pepper).ToString(), WorkFactor);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+            if (string.IsNullOrEmpty(hashedPassword))
+                throw new ArgumentNullException(nameof(hashedPassword));
+
+            return BCrypt.Net.BCrypt.EnhancedVerify(password.Concat(_pepper).ToString(), hashedPassword);
+        }
+    }
+}
