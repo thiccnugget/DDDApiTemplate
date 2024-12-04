3b908509c42b99f1a533679a04fabae10f0dc08e
thiccnugget <samuelecammaraa@gmail.com>
2024-12-04 23:31:54 +0100

    feat: added jwt authentication and middleware for authorization policies

---
+++ TestApi/Services/ConfigureAuth.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace TestApi.Services
+{
+    internal static class ConfigureAuthExtension
+    {
+        public static IServiceCollection ConfigureAuth(this IServiceCollection services, IConfiguration config)
+        {
+            // Configure JWT authentication
+            var jwtSettings = config.GetSection("JwtSettings");
+            string? secretKey = jwtSettings.GetValue<string>("SecretKey");
+
+            if(string.IsNullOrEmpty(secretKey))
+            {
+                throw new InvalidOperationException("JWT Secret key is missing");
+            }
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetValue<string>("Issuer"),
+                    ValidAudience = jwtSettings.GetValue<string>("Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    AuthenticationType = "Bearer"
+                };
+            });
+
+            // Add authorization policies as needed
+            services.AddAuthorization(x => x.AddPolicy("AdminPolicy", p => p.RequireRole("Admin")));
+
+            return services;
+        }
+    }
+}
